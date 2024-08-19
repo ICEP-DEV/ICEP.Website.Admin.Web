b85e72d4ae18f9ff01b9e6d9e0e0073147ebfade
Dipono Joel Manasoe <ManasoeDJ@tut.ac.za>
2024-08-19 14:06:45 +0200

    add new application

--- src/screens/Dashboard.jsx
+++ src/screens/Dashboard.jsx
@@ -367,27 +367,12 @@ function Dashboard() {
                                 <label >Delete</label>
                             </>
                             }
-
                         </Card.Body>
                     ))}
-
                 </Card>
             </div>
         </div>
 
-
-
-        {/* <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>  */}
 
     </div>)
 
